7d8698d7fca7273ab3bffd63a6d07b318538551e
Jeff Johnson <jeff@gridfinity.com>
2021-01-03 22:27:47 -0500

    Happy New Year!

--- leaktestfe_disable_test.go
+++ leaktestfe_disable_test.go
@@ -1,5 +1,5 @@
-// Copyright 2020 Jeffrey H. Johnson.
-// Copyright 2020 Gridfinity, LLC.
+// Copyright 2021 Jeffrey H. Johnson.
+// Copyright 2021 Gridfinity, LLC.
 // Copyright 2019 The Go Authors.
 // All rights reserved.
 // Use of this source code is governed by the BSD-style
--- leaktestfe_enable_test.go
+++ leaktestfe_enable_test.go
@@ -1,5 +1,5 @@
-// Copyright 2020 Gridfinity, LLC.
-// Copyright 2019 The Go Authors.
+// Copyright 2021 Gridfinity, LLC.
+// Copyright 2020 The Go Authors.
 // All rights reserved.
 // Use of this source code is governed by the BSD-style
 // license that can be found in the LICENSE file.
